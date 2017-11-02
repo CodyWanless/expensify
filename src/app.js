5929ebad368e1861e32a34d5620ae53bf9f04e88
Wanless <wanlessc@mfldclin.org>
2017-11-02 11:50:22 -0500

    Clean up app.js

--- src/app.js
+++ src/app.js
@@ -3,20 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 121, note: '', createdAt: 5 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 1234, note: '', createdAt: 1 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 1095, note: '', createdAt: new Date().getTime() }));
-
-const currentState = store.getState();
 
 const jsx = (
     <Provider store={store}>
